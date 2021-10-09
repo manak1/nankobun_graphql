d13fbd450ef4cc95858c395637e631c9c4c9438c
manak1 <mana23happy@hotmail.co.jp>
2021-10-10 03:30:11 +0900

    :sparkles: feat: set typeorm setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,24 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SindansModule } from './sindans/sindans.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'nankobun_b',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    SindansModule,
+  ],
 })
 export class AppModule {}
